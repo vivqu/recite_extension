d4b399629dfd685e43c7429889d4101bc0fbc6b5
Vivian Qu <vivian@pathlight.com>
2022-12-31 22:19:29 -0800

    Launch the settings page on install

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
-chrome.action.onClicked.addListener(function() {
-    chrome.tabs.create({url: 'settings.html'});
-  });
+chrome.action.onClicked.addListener(function () {
+  chrome.tabs.create({ url: "settings.html" });
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason == "install") {
+    chrome.tabs.create({ url: "settings.html" });
+  }
+});
